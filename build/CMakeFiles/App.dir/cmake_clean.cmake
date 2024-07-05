d53b15692bc36f94c2216732ebbaa6cc5d8784fe
Gfandrade10 <gfandrade10@hotmail.com>
2024-07-05 18:14:38 -0300

    test box

---
+++ build/CMakeFiles/App.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/App.dir/test.cpp.obj"
+  "CMakeFiles/App.dir/test.cpp.obj.d"
+  "F:/C_C++ coding/NVim Training/App.exe"
+  "F:/C_C++ coding/NVim Training/App.exe.manifest"
+  "F:/C_C++ coding/NVim Training/App.pdb"
+  "libApp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/App.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
